c839d0265d91866ab6d97e388f98bee292d17825
kivitoz <kivitoz@yahoo.com.ar>
2020-06-29 09:48:29 -0300

    Seteo de Acces Token, Collector ID y Country Id Seteo de DNI en comment index Cambios para la creación del store, pos y Orden Verificación de las leyendas Revisa Aquí Modificación de Rutas/URL en llamadas ajax y Rest API

---
+++ api/order/delete/index.php
@@ -0,0 +1,22 @@
+<?php
+
+// Borrar o cancelar una orden creada
+
+header('Content-type: application/json');
+
+include_once '../../global/functions.php';
+global $access_token, $collector_id;
+
+ $external_id = $_REQUEST["external_id"];
+ $json = "{}";
+
+ // REVISA AQUÍ:(Ok)
+ // Qué método y endpoint de la API de Mercado Pago deberías poner aquí para poder eliminar una orden
+ // en base al external_id, collector_id recibidos
+ // Pista... revísalo bien...
+ // Sustituye el método por su correspondiente: get, put, post, delete
+ $url = "https://api.mercadopago.com/mpmobile/instore/qr";
+
+curl_call("DELETE","$url/$collector_id/$external_id?access_token=$access_token",$json);
+
+?>
---
+++ api/pos/create/index.php
@@ -0,0 +1,22 @@
+<?php
+
+// Crea el pos
+
+header('Content-type: application/json');
+
+include_once '../../global/functions.php';
+global $access_token;
+
+ // REVISA AQUÍ:(OK)
+ // Qué método y endpoint de la API de Mercado Pago deberías poner aquí para poder   
+ // crear un POS/QR?
+ // Sustituye el método por su correspondiente: get, put, post, delete
+
+
+$json = $_POST["json"];
+$url ="https://api.mercadopago.com";
+
+
+curl_call("POST","$url/pos?access_token=$access_token",$json);
+
+?>
