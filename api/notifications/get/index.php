f2d0f1c8dde3a01ab6304c6afd0819a652ad3912
kivitoz <kivitoz@yahoo.com.ar>
2020-06-29 18:07:39 -0300

    Cambios varios

--- api/notifications/get/index.php
+++ api/notifications/get/index.php
@@ -11,13 +11,13 @@ global $access_token, $collector_id, $notificationJSON;
 
 $lastResource = file_get_contents('../notifications.txt');
 
-$url= "https://hookbin.com/DrGJkXnnPnFdNNEweNm7";
+$url= "../notifications.txt";
 
 // REVISAR AQUÍ:
 // Agrega la $url necesaria para revisar el estado del pago en base al recurso recibido de la notificación
 
 // Sustituye el método por su correspondiente: get, put, post, delete
 
-curl_call("GET",$url,"{'name': '29131999'}");
+curl_call("get",$url,"");
 
  ?>
